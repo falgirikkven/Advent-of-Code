cbe6d42b80c06fc0843ec3ba3b33c57df9867e95
Falgirikkven <leo_93_t1@hotmail.com>
2025-12-01 21:49:35 -0300

    added part two to 2024 day 17

--- 2024/day-17/day-17.js
+++ 2024/day-17/day-17.js
@@ -1,110 +1,113 @@
-const fs = require("fs");
-const path = require("path");
-const INPUT = fs
-    .readFileSync(path.resolve(__dirname, "./input.txt"), "utf8")
-    .split("\r\n");
+const fs = require("node:fs");
+const path = require("node:path");
+const input = fs
+  .readFileSync(path.resolve(__dirname, "./input"), "utf8")
+  .trim();
 
-function parseInput() {
-    const input = INPUT;
-    const regex = /(\d+)/g;
-    let A = Number(input[0].match(regex)[0]);
-    let B = Number(input[1].match(regex)[0]);
-    let C = Number(input[2].match(regex)[0]);
-    const registerA = A;
-    const registerB = B;
-    const registerC = C;
-    const program = input[4].match(regex).map((str) => Number(str));
-    return { A, B, C, registerA, registerB, registerC, program };
-}
+const parseData = (data) => {
+  const regex = /(\d+)/g;
+  const splitedData = data.split("\n");
+  const registers = {};
+  registers.A = Number(splitedData[0].match(regex)[0]);
+  registers.B = Number(splitedData[1].match(regex)[0]);
+  registers.C = Number(splitedData[2].match(regex)[0]);
+  const program = splitedData[4].match(regex).map((e) => Number(e));
+  const output = [];
+  return { registers, program, output };
+};
 
-const data = parseInput();
+const { registers, program, output } = parseData(input);
+let instruction = 0;
 
 const operations = new Map([
-    [0, (o) => Math.floor(data.A / Math.pow(2, combo(o)))],
-    [1, (o) => data.B ^ o],
-    [2, (o) => combo(o) % 8],
-    [3, (o) => (data.A == 0 ? -1 : o)],
-    [4, (_) => data.B ^ data.C],
-    [5, (o) => combo(o) % 8],
-    [6, (o) => Math.floor(data.A / Math.pow(2, combo(o)))],
-    [7, (o) => Math.floor(data.A / Math.pow(2, combo(o)))],
+  [0, (o) => Math.floor(registers.A / Math.pow(2, combo(o)))],
+  [1, (o) => registers.B ^ o],
+  [2, (o) => combo(o) % 8],
+  [3, (o) => (registers.A == 0 ? -1 : o)],
+  [4, (_) => registers.B ^ registers.C],
+  [5, (o) => combo(o) % 8],
+  [6, (o) => Math.floor(registers.A / Math.pow(2, combo(o)))],
+  [7, (o) => Math.floor(registers.A / Math.pow(2, combo(o)))],
 ]);
 
 function combo(literal) {
-    if (literal == 4) return data.A;
-    else if (literal == 5) return data.B;
-    else if (literal == 6) return data.C;
-    return literal;
+  if (literal == 4) return registers.A;
+  else if (literal == 5) return registers.B;
+  else if (literal == 6) return registers.C;
+  return literal;
 }
 
+const saveRegister = (opcode, res) => {
+  if (opcode === 0) registers.A = res;
+  else if (opcode === 1) registers.B = res;
+  else if (opcode === 2) registers.B = res;
+  // opcode === 3 alter instruction pointer
+  else if (opcode === 4) registers.B = res;
+  else if (opcode === 5) output.push(res);
+  else if (opcode === 6) registers.B = res;
+  else if (opcode === 7) registers.C = res;
+};
+
 function partOne() {
-    const { program } = data;
-    const max = program.length - 1;
-    const out = [];
-    let cursor = 0;
-    while (cursor < max) {
-        const opcode = program[cursor];
-        const operand = program[cursor + 1];
-        const res = operations.get(opcode)(operand);
-        if (opcode == 0) {
-            data.A = res;
-            cursor += 2;
-        } else if (opcode == 1 || opcode == 2 || opcode == 4 || opcode == 6) {
-            data.B = res;
-            cursor += 2;
-        } else if (opcode == 7) {
-            data.C = res;
-            cursor += 2;
-        } else if (opcode == 5) {
-            out.push(res);
-            cursor += 2;
-        } else if (opcode == 3) {
-            if (cursor != res && res != -1) {
-                cursor = res;
-            } else {
-                cursor += 2;
-            }
-        }
-    }
-    return out.join(",");
+  output.length = 0;
+  instruction = 0;
+  let haltValue = program.length;
+  while (instruction < haltValue) {
+    let opcode = program[instruction];
+    let operand = program[instruction + 1];
+    const restult = operations.get(opcode)(operand);
+
+    saveRegister(opcode, restult);
+
+    if (opcode == 3 && restult != -1) instruction = restult;
+    else instruction += 2;
+  }
+
+  return output;
+}
+
+// This function is deduced after analyzing the input of my puzzle
+const magic_number = 8;
+function magic_function(a) {
+  let A = a;
+  let B = BigInt(0);
+  let C = BigInt(0);
+
+  B = A % BigInt(8) ^ BigInt(3);
+  C = A >> B;
+  A = A / BigInt(8);
+  B = B ^ C ^ BigInt(5);
+  const o = B % BigInt(8);
+
+  return o;
+}
+
+function dfs(regA, digit, founds) {
+  if (magic_function(regA) !== BigInt(program[program.length - digit])) return;
+  const newA = regA * BigInt(magic_number);
+
+  if (digit === program.length) {
+    founds.push(regA);
+    return;
+  }
+
+  for (let i = 0; i < magic_number; i++) {
+    dfs(newA + BigInt(i), digit + 1, founds);
+  }
 }
 
 function partTwo() {
-    const out = [];
-    const { registerA, registerB, registerC, program } = data;
-    let res = BigInt(-1);
-    let char = 1;
-
-    let b = true;
-    while (b) {
-        res += BigInt(1);
-        let A = res;
-        let B = BigInt(registerB);
-        let C = BigInt(registerC);
-        do {
-            B = A % BigInt(8) ^ BigInt(3);
-            C = A >> B;
-            A = A / BigInt(8);
-            B = B ^ C ^ BigInt(5);
-            out.push(B % BigInt(8));
-        } while (A != 0);
-        if (out.at(-char) != program.at(-char)) {
-            out.length = 0;
-            continue;
-        }
-        if (out.length == program.length) break;
-
-        res *= BigInt(8);
-        char += 1;
-    }
-    console.log(out);
-    return res;
+  const founds = [];
+  for (let i = 0; i < magic_number; i++) {
+    dfs(BigInt(i), 1, founds);
+  }
+  return founds.sort()[0];
 }
 
-// 1,5,0,5,2,0,1,3,5
-//const firstPart = partOne();
-//console.log("Part One", firstPart);
+const firstPart = partOne();
+console.log("Part One", firstPart);
+// Expected output: 1,5,0,5,2,0,1,3,5
 
-// TODO
 const secondPart = partTwo();
 console.log("Part Two", secondPart);
+// Expected output: 236581108670061
